e8ad05c0ae28087d1ca7fdd652a81b439580f4e8
jcorderog <alum.jcorderog@iesalixar.org>
2020-09-24 15:29:10 +0200

    Colores definidos

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,7 +1,10 @@
 public class HolaMundo {
-	public static void main(String[] args) {		
-		System.out.println("Hola Mundo");
-		System.out.println("Hola Mundo");
+	public static void main(String[] args) {	
+		String naranja = "\033[33m";
+		String azul = "\033[34m";
+			
+		System.out.println(naranja + "¡Hola Mundo!");
+		System.out.println(azul + "Aquí estamos haciedno preubas con Git.");
 	}
 }
 
